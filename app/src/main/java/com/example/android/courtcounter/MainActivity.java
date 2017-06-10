75378d88d889dc7008b0c83d05b0acfa19d40818
Reggie Lyons <reglyons@gmail.com>
2017-06-10 17:56:55 +0100

    Adding proper landscape variations for each layout/ Deleted all unused files. Tidied up code throughout all files.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -13,24 +13,28 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     //Initialise ints to track team scores and TextViews to display them.
-    int scoreA = 0;
-    int scoreB = 0;
-    TextView scoreViewA, scoreViewB;
+    private int scoreA = 0;
+    private int scoreB = 0;
+    private TextView scoreViewA, scoreViewB;
 
-    //Lawlers law tracker
-    boolean lawlersLaw = false;
+    /***
+     * Lawler's Law tracker. Lawler's Law is a term first introduced by Los Angeles Clippers
+     * broadcaster Ralph Lawler.
+     * @see <a href="http://lawlerslawtracker.com/">Lawler's Law Tracker Website</a>
+     */
+    private boolean lawlersLaw = false;
 
 
     //Initialise ImageViews to display selected team's logo
-    ImageView logoA, logoB;
+    private ImageView logoA, logoB;
 
     //States for recalling scores on restore
-    static final String STATE_SCORE_A = "ScoreTeamA";
-    static final String STATE_SCORE_B = "ScoreTeamB";
-    static final String STATE_LAWLER = "LawlersLaw";
+    private static final String STATE_SCORE_A = "ScoreTeamA";
+    private static final String STATE_SCORE_B = "ScoreTeamB";
+    private static final String STATE_LAWLER = "LawlersLaw";
 
     //Initialise an array of NBA teams.
-    String teams[] = {
+    private final String teams[] = {
             "Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets",
             "Chicago Bulls", "Cleveland Cavaliers", "Dallas Mavericks", "Denver Nuggets",
             "Detroit Pistons", "Golden State Warriors", "Houston Rockets", "Indiana Pacers",
@@ -42,14 +46,14 @@ public class MainActivity extends AppCompatActivity {
     };
 
     //Initialise the Spinners to display all the teams for selection.
-    Spinner spinnerA, spinnerB;
+    private Spinner spinnerA, spinnerB;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             scoreA = savedInstanceState.getInt(STATE_SCORE_A);
             scoreB = savedInstanceState.getInt(STATE_SCORE_B);
             lawlersLaw = savedInstanceState.getBoolean(STATE_LAWLER);
@@ -97,14 +101,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //The following two methods update the scores for each team.
-    public void updateScoreA(View btn){
-        scoreA+=Integer.parseInt(btn.getTag().toString());
+    public void updateScoreA(View btn) {
+        scoreA += Integer.parseInt(btn.getTag().toString());
         displayScore(scoreA, scoreViewA);
         checkScoreForLawlersLaw();
     }
 
-    public void updateScoreB(View btn){
-        scoreB+=Integer.parseInt(btn.getTag().toString());
+    public void updateScoreB(View btn) {
+        scoreB += Integer.parseInt(btn.getTag().toString());
         displayScore(scoreB, scoreViewB);
         checkScoreForLawlersLaw();
 
@@ -116,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Reset the scores to 0
-    public void resetScores(View resetBtn){
+    public void resetScores(View resetBtn) {
         scoreA = 0;
         scoreB = 0;
         lawlersLaw = false;
@@ -124,15 +128,16 @@ public class MainActivity extends AppCompatActivity {
         displayScore(scoreB, scoreViewB);
     }
 
-    private void checkScoreForLawlersLaw(){
-        if((scoreA >= 100 || scoreB >= 100) && !lawlersLaw){
-            Toast.makeText(MainActivity.this, "Lawler's law, first to 100 wins, that's the law!",  Toast.LENGTH_SHORT).show();
+    //A quick method to check if Lawler's Law has been met, displaying it to the user if it has.
+    private void checkScoreForLawlersLaw() {
+        if ((scoreA >= 100 || scoreB >= 100) && !lawlersLaw) {
+            Toast.makeText(MainActivity.this, getString(R.string.lawlers_law), Toast.LENGTH_SHORT).show();
             lawlersLaw = true;
         }
     }
 
     //Add teams to spinners
-    public void addToSpinner(Spinner spin, String[] list) {
+    private void addToSpinner(Spinner spin, String[] list) {
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, list);
@@ -141,44 +146,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Get the selected dropdown list value
-    public void addListenerOnSpinnerItemSelection(Spinner spin) {
-
+    private void addListenerOnSpinnerItemSelection(Spinner spin) {
         spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
                 setTeamLogos();
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
-                // sometimes you need nothing here
+                // Nothing necessary here.
             }
-
         });
     }
 
     //Set the logo based on the selected item in the dropdown menu
-    public void setTeamLogos(){
+    private void setTeamLogos() {
         logoA.setImageResource(teamLogos[spinnerA.getSelectedItemPosition()]);
         logoB.setImageResource(teamLogos[spinnerB.getSelectedItemPosition()]);
     }
 
     //Array of references to drawable team logos
-    private Integer[] teamLogos = {
+    private final Integer[] teamLogos = {
             R.drawable.logo1, R.drawable.logo2, R.drawable.logo3, R.drawable.logo4,
             R.drawable.logo5, R.drawable.logo6, R.drawable.logo7, R.drawable.logo8,
             R.drawable.logo9, R.drawable.logo10, R.drawable.logo11, R.drawable.logo12,
             R.drawable.logo13, R.drawable.logo14, R.drawable.logo15, R.drawable.logo16,
             R.drawable.logo17, R.drawable.logo18, R.drawable.logo19, R.drawable.logo20,
-            R.drawable.logo21, R.drawable.logo22, R.drawable.logo22, R.drawable.logo24,
+            R.drawable.logo21, R.drawable.logo22, R.drawable.logo23, R.drawable.logo24,
             R.drawable.logo25, R.drawable.logo26, R.drawable.logo27, R.drawable.logo28,
             R.drawable.logo29, R.drawable.logo30
     };
-
-
 }
 
 
